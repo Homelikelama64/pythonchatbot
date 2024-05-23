5368417f9ab5f3aeec3f7abc89d774ca062f78fc
Homelikelama64 <homelikelama64@gmail.com>
2024-05-24 11:02:16 +1200

    Clears Terminal after every option

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
+
 backMessage = "To go back Press anything"
 
+def clear():
+    os.system('cls')
+
 def lsb():
+    clear()
     print("""
     LSB is the least significant bit in a binary number
 	press 1 to go back
@@ -12,6 +18,7 @@ def lsb():
 
 
 def multiplication():
+    clear()
     print("""
 Multiplication is trivial in binary
 using the shift and add method
@@ -53,6 +60,7 @@ Here is a Diagram For Unsigned multiplication
 
 
 def bitshifting():
+    clear()
     print("Bitshifting is when you shift the bits in a binary number. say you got 01100(12) if you bitshift it 1 to the right it becomes 00110(6), shifting it to the left you get 11000(24). Notice that when you bitshift you ether double or half the value. using this we can use this to multiply using the shift and add method.")
     print(backMessage)
     option = input()
@@ -60,6 +68,7 @@ def bitshifting():
         binarybaiscs()
 
 def andoperator():
+    clear()
     print("The And operator compares to numbers and gives a new number witch only has the bits that are on for both e.g.\n   1101110\n& 0110010\nwill give you 0100010.")
     print(backMessage)
     option = input()
@@ -67,6 +76,7 @@ def andoperator():
         binarybaiscs()
 
 def binarybaiscs():
+    clear()
     print("The usual number system that you use for english base 10 but computers use base 2(Binary) so all data is represented by on and off. For this I will represent 1           as on and 0 as off. Ten is represented like this 10 in base 10 but in base 2(Binary) Ten is represented as 1010. To covert base 2 to base 10. you multiply the digits like this\n(8*1)+(4*0)+(2*1)+(1*0)\n=(8*1)+(2*1)\n=8+2\n=10.\nevery Digit in base 2 is 2 times the amout as the last digit. the formula would be(starting at 0) 2^d to get how much that digit contributes in base 10.\nThats the basics of binary\nFor what bitshifting is Press 1\nFor what the AND operator is press 2\nFor how to multiply two numbers press 3\nFor LSB press 4\nTo go back press 5")
     option = input()
     if option == "1":
@@ -80,13 +90,15 @@ def binarybaiscs():
     elif option == "5":
         intro()
     else:
-        option = input()
+        clear()
         print("Not an Option")
         print(backMessage)
+        option = input()
         if option:
             binarybaiscs()
 
 def intro():
+    clear()
     print("Intro: What do you want to Learn About\nPress 1 for : How Binary works and various binary operations\nPress 2 for : For the two types of Computer Architure(CISC,RISC)\nPress 3 for : Going About Creating Your Own CPU")
     option = input()
     if option == "1":
